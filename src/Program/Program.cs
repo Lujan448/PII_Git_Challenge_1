9f17c8e6788f15a56da31c77a7682cbf143e4ff6
Joaquin Hernandez <hzjoaquin287@gmail.com>
2026-03-16 18:47:22 -0300

    Update naming

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -28,11 +28,10 @@ public class Addition
 }
 
 // Esta clase implementa la operación <division>
-public class division
+public class Division
 {
    public static double Divide(int a, int b)
    {
        return (double)a / b;
    }
 }
-git stash
