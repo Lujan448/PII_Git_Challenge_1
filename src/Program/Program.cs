a691fbacc2d52307102c12a7007ff30281e64fee
Diegol <diegolabrea9@gmail.com>
2026-03-16 19:10:41 -0300

    arreglo de multiplicacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,9 +7,9 @@
         Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
         Console.WriteLine(Division.Dividir(7, 8));
     }
-    public class Multiplication
+    public class Multiplicacion
 {
-   public static int Multiply(int a, int b)
+   public static int Multiplicar(int a, int b)
    {
        return a * b;
     }
