e309c7e6526d1ee31eb1a0d3032b8c89807b17b6
Diegol <diegolabrea9@gmail.com>
2026-03-16 19:05:06 -0300

    cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,4 +7,11 @@
         Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
         Console.WriteLine(Division.Dividir(7, 8));
     }
+    public class Multiplication
+{
+   public static int Multiply(int a, int b)
+   {
+       return a * b;
+    }
+}
 }
