c884286a1a16c844de9c67a71d17c412c6bc54c1
feerbb <fernanda.delasierra@correo.ucu.edu.uy>
2026-03-16 18:55:34 -0300

    Comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,3 +8,11 @@
         Console.WriteLine(Division.Dividir(7, 8));
     }
 }
+//Esta clase implementa la operación resta
+public class Resta
+{
+   public static int Restar(int a, int b)
+   {
+       return a - b;
+   }
+}
